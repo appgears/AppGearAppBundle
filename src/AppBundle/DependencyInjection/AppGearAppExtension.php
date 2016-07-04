286abf7d776932ffa5d79d58bab4be1e04f8f3b3
Your Name <you@example.com>
2016-07-04 18:29:38 +0300

    redirect to home page when 404

--- src/AppBundle/DependencyInjection/AppGearAppExtension.php
+++ src/AppBundle/DependencyInjection/AppGearAppExtension.php
@@ -14,8 +14,14 @@ class AppGearAppExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config        = $this->processConfiguration($configuration, $configs);
+
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('entities.yml');
         $loader->load('services.yml');
+
+        $container->setParameter('appgear.application.route404.enabled', $config['route404']['enabled']);
+        $container->setParameter('appgear.application.route404.route', $config['route404']['route']);
     }
 }
---
+++ src/AppBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppGear\AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * AppGearAppBundle configuration
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode    = $treeBuilder->root('app_gear_app');
+
+        $rootNode
+            ->children()
+                ->arrayNode('route404')
+                    ->children()
+                        ->booleanNode('enabled')
+                            ->defaultFalse()
+                        ->end()
+                        ->scalarNode('route')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
