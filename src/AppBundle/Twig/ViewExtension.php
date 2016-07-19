09de5fb82d8c65298b413ba7133584a2c1564085
Your Name <you@example.com>
2016-07-19 19:16:59 +0300

    markdown_typorgraph dev - process html

--- src/AppBundle/Twig/ViewExtension.php
+++ src/AppBundle/Twig/ViewExtension.php
@@ -5,6 +5,10 @@ namespace AppGear\AppBundle\Twig;
 use AppGear\AppBundle\Entity\View;
 use AppGear\AppBundle\View\ViewManager;
 use League\CommonMark\CommonMarkConverter;
+use simplehtmldom_1_5\simple_html_dom;
+use simplehtmldom_1_5\simple_html_dom_node;
+use Sunra\PhpSimple\HtmlDomParser;
+use Symfony\Component\DomCrawler\Crawler;
 use Twig_Extension;
 use Twig_SimpleFilter;
 
@@ -63,7 +67,41 @@ class ViewExtension extends Twig_Extension
     public function markdownTypograph($markdown)
     {
         $converter = new CommonMarkConverter();
-        $html = $converter->convertToHtml($markdown);
+        $html      = $converter->convertToHtml($markdown);
+
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($html, 'UTF-8');
+
+        foreach ($crawler->filter('p img') as $image) {
+            /** @var \DomElement $image */
+            /** @var \DomElement $paragraph */
+            if (($paragraph = $image->parentNode) && ($paragraph->childNodes->length !== 1)) {
+                continue;
+            }
+
+            $div = new \DOMElement('div');
+            $paragraph->parentNode->replaceChild($div, $paragraph);
+            $div->setAttribute('class', 'article-image');
+
+            $div->appendChild($image);
+
+            // if image has alt text - add paragraph with this text
+            if ($alt = $image->getAttribute('alt')) {
+                $legend = new \DOMElement('p', $alt);
+                $div->appendChild($legend);
+            }
+        }
+
+        $html = '';
+        foreach ($crawler->getNode(0)->childNodes->item(0)->childNodes as $childNode) {
+            $html .= $childNode->ownerDocument->saveXML($childNode);
+        }
+
+        // Fix bug with encoded image alt-text
+        preg_match_all('/alt="(.+?)"/', $html, $matches);
+        foreach ($matches[1] as $match) {
+            $html = str_replace($match, html_entity_decode($match), $html);
+        }
 
         return $html;
     }
