08966d0caed6bf9c1f3262efc5b28f0ec874a90a
Your Name <you@example.com>
2018-05-18 20:31:10 +0300

    ACL class-field-scope implementation

---
+++ src/AppBundle/Command/AclSetClassFieldScopeCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace AppGear\AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Exception\InvalidOptionException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Exception\AclNotFoundException;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class AclSetClassFieldScopeCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('appgear:app:acl:set:class-field-scope')
+            ->setDescription("Setup ACLs and ACEs for Class-Field-Scope access demo.\nExample:\nappgear:app:acl:set:class-field-scope --fqcn=\"Commerce\PlatformBundle\Entity\Shop\Order\" --field=\"status\" --permission=\"VIEW\" --username=\"khz\" --user-provider=\"in_memory\"")
+            ->addOption('fqcn', null, InputOption::VALUE_REQUIRED, 'Target entity FQCN')
+            ->addOption('field', null, InputOption::VALUE_REQUIRED, 'Target entity field name')
+            ->addOption('permission', null, InputOption::VALUE_REQUIRED, 'Permission (VIEW, C - CREATE, E - EDIT, D - DELETE etc. See all codes in the MaskBuilder class)')
+            ->addOption('username', null, InputOption::VALUE_REQUIRED, 'User name')
+            ->addOption('user-provider', null, InputOption::VALUE_REQUIRED, 'User provider (in_memory etc.)');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fqcn         = $input->getOption('fqcn');
+        $field        = $input->getOption('field');
+        $userName     = $input->getOption('username');
+        $permission   = $input->getOption('permission');
+        $userProvider = $input->getOption('user-provider');
+
+        if (!$fqcn) {
+            throw new InvalidOptionException('Set "fqcn" option');
+        }
+        if (!$field) {
+            throw new InvalidOptionException('Set "field" option');
+        }
+        if (!$userName) {
+            throw new InvalidOptionException('Set "username" option');
+        }
+        if (!$permission) {
+            throw new InvalidOptionException('Set "permission" option');
+        }
+        if (!$userProvider) {
+            throw new InvalidOptionException('Set "user-provider" option');
+        }
+
+        $aclProvider = $this->getContainer()->get('security.acl.provider');
+
+        $oid = new ObjectIdentity('class', $fqcn . 'l');
+        
+        try {
+            $acl = $aclProvider->findAcl($oid);
+        } catch (AclNotFoundException $e) {
+            $acl = $aclProvider->createAcl($oid);
+        }
+
+        // Role based securityIdentity
+        //$securityIdentity = new RoleSecurityIdentity("ROLE_POST_OWNER");
+
+        /** @var UserProviderInterface $securityUserProvider */
+        $securityUserProvider = $this->getContainer()->get('security.user.provider.concrete.' . $userProvider);
+        $user                 = $securityUserProvider->loadUserByUsername($userName);
+        $securityIdentity     = UserSecurityIdentity::fromAccount($user);
+
+        $maskBuilder = new MaskBuilder();
+        $mask        = $maskBuilder->resolveMask($permission);
+
+        $acl->insertClassFieldAce($field, $securityIdentity, $mask);
+        $aclProvider->updateAcl($acl);
+
+        return 0;
+    }
+}
--- src/AppBundle/Security/SecurityManager.php
+++ src/AppBundle/Security/SecurityManager.php
@@ -5,6 +5,7 @@ namespace AppGear\AppBundle\Security;
 use AppGear\CoreBundle\Model\ModelManager;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Voter\FieldVote;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
@@ -63,22 +64,34 @@ class SecurityManager
      *
      * @return bool
      */
-    public function check(string $permission, string $model, $id = null): bool
+    public function check(string $permission, string $model, $id = null, $field = null): bool
     {
         $fqcn = $this->modelManager->fullClassName($model);
         $id   = $id ?? self::CLASS_SCOPE_OBJECT_IDENTIFIER_VALUE;
 
+        // Check access to object
         $objectIdentity = new ObjectIdentity($id, $fqcn);
+        $isGranted      = $this->checker->isGranted($permission, $objectIdentity);
 
-        $isGranted = $this->checker->isGranted($permission, $objectIdentity);
-
-        if (!$isGranted) {
-            $token    = $this->tokenStorage->getToken();
-            $username = ($token !== null) ? $token->getUsername() : null;
-
-            $this->logger->notice('Access denied', compact('username', 'permission', 'model', 'id'));
+        if ($isGranted) {
+            return true;
         }
 
-        return $isGranted;
+        // Check access to field
+        if ($field !== null) {
+            $fieldVote = new FieldVote($objectIdentity, $field);
+            $isGranted = $this->checker->isGranted($permission, $fieldVote);
+
+            if ($isGranted) {
+                return true;
+            }
+        }
+
+        // Log access denied
+        $token    = $this->tokenStorage->getToken();
+        $username = ($token !== null) ? $token->getUsername() : null;
+        $this->logger->notice('Access denied', compact('username', 'permission', 'model', 'id'));
+
+        return false;
     }
 }
