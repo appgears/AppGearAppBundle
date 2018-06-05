4b210d83ccaa48d0305c3526ed48425001a1cbf8
Your Name <you@example.com>
2018-06-05 19:57:52 +0300

    Core: IntegerType service supports FormFieldTypeServiceInterface

--- src/AppBundle/EntityService/Property/Field/IntegerTypeService.php
+++ src/AppBundle/EntityService/Property/Field/IntegerTypeService.php
@@ -2,10 +2,28 @@
 
 namespace AppGear\AppBundle\EntityService\Property\Field;
 
+use AppGear\AppBundle\Form\FormFieldTypeServiceInterface;
 use AppGear\AppBundle\Storage\Platform\MysqlFieldTypeServiceInterface;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
-class IntegerTypeService implements MysqlFieldTypeServiceInterface
+class IntegerTypeService implements FormFieldTypeServiceInterface, MysqlFieldTypeServiceInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormType()
+    {
+        return IntegerType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormOptions()
+    {
+        return [];
+    }
+
     /**
      * {@inheritdoc}
      */
